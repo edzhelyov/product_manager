35f63cfe3a8ff6f45415d19c689ad77cb05ad3b6
Evgeni Dzhelyov <evgeni.dzhelyov@gmail.com>
2011-09-16 17:26:42 +0300

    Remove direct requires and rely on Rails autoloading

--- lib/product_manager.rb
+++ lib/product_manager.rb
@@ -1,9 +1,3 @@
-#require_relative 'models/product_type'
-#require_relative 'models/product_attribute_type'
-#require_relative 'models/product_attribute_value'
-#require_relative 'models/product_attribute'
-#require_relative 'models/product'
-
 require_relative 'product_manager/engine'
 
 module ProductManager

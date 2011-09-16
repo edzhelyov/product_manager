eda1926286c8d909b4d9b065082adb216b6ce754
Evgeni Dzhelyov <evgeni.dzhelyov@gmail.com>
2011-09-16 14:27:55 +0300

    Moved the model files so they are autoloaded by Rails

--- lib/product_manager.rb
+++ lib/product_manager.rb
@@ -1,8 +1,10 @@
-require_relative 'models/product_type'
-require_relative 'models/product_attribute_type'
-require_relative 'models/product_attribute_value'
-require_relative 'models/product_attribute'
-require_relative 'models/product'
+#require_relative 'models/product_type'
+#require_relative 'models/product_attribute_type'
+#require_relative 'models/product_attribute_value'
+#require_relative 'models/product_attribute'
+#require_relative 'models/product'
+
+require_relative 'product_manager/engine'
 
 module ProductManager
 end
---
+++ lib/product_manager/engine.rb
@@ -0,0 +1,4 @@
+module ProductManager
+  class Engine < ::Rails::Engine
+  end
+end

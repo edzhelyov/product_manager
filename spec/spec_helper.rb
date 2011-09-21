c9a339cb6bfbeee86477a99be21bfbe9dbcb9fad
Evgeni Dzhelyov <evgeni.dzhelyov@gmail.com>
2011-09-21 15:48:11 +0300

    Add format and color options

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -27,4 +27,6 @@ Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 
 RSpec.configure do |config|
   config.use_transactional_fixtures = true
+  config.color_enabled = true
+  config.formatter = :doc
 end

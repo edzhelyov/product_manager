c60864361136933c4b2300db26e7e2dcd5cf5571
Evgeni Dzhelyov <evgeni.dzhelyov@gmail.com>
2011-09-28 19:36:26 +0300

    Replace the klass variable with more meaningful name

--- app/models/product_attribute_type.rb
+++ app/models/product_attribute_type.rb
@@ -4,7 +4,7 @@ class ProductAttributeType < ActiveRecord::Base
   # Casts value (which is a String) to an appropriate instance.
   # This is basically a copy of the same method in AR::ConnectionAdapters::Column
   def type_cast(value)
-    klass = ActiveRecord::ConnectionAdapters::Column
+    converter = ActiveRecord::ConnectionAdapters::Column
 
     return nil if value.nil?
     case data_type.to_sym
@@ -12,13 +12,13 @@ class ProductAttributeType < ActiveRecord::Base
     when :text      then value
     when :integer   then value.to_i rescue value ? 1 : 0
     when :float     then value.to_f
-    when :decimal   then klass.value_to_decimal(value)
-    when :datetime  then klass.string_to_time(value)
-    when :timestamp then klass.string_to_time(value)
-    when :time      then klass.string_to_dummy_time(value)
-    when :date      then klass.string_to_date(value)
-    when :binary    then klass.binary_to_string(value)
-    when :boolean   then klass.value_to_boolean(value)
+    when :decimal   then converter.value_to_decimal(value)
+    when :datetime  then converter.string_to_time(value)
+    when :timestamp then converter.string_to_time(value)
+    when :time      then converter.string_to_dummy_time(value)
+    when :date      then converter.string_to_date(value)
+    when :binary    then converter.binary_to_string(value)
+    when :boolean   then converter.value_to_boolean(value)
     else value
     end
   end
